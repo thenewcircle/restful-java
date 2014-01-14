18fb7c6587d627c554b7805ad56df39e6bd86041
Gordon Force <Gordon Force@171.69.52.109>
2014-01-13 16:53:37 -0800

    Added support for clearing the user repoistory between test method invocations.

--- src/test/java/chirp/service/resources/UserResourceTest.java
+++ src/test/java/chirp/service/resources/UserResourceTest.java
@@ -6,9 +6,19 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Form;
 import javax.ws.rs.core.Response;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import chirp.model.UserRepository;
+
 public class UserResourceTest extends JerseyResourceTest<UserResource> {
+	
+	UserRepository userRepository = UserRepository.getInstance();
+	
+	@Before 
+	public void clearRepository() {
+		userRepository.clear();
+	}
 
 	@Test
 	public void createUserSuccess() {
