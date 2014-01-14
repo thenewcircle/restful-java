e98eead0cc51f31f40665902113ba3a7f7beb74a
Gordon Force <Gordon Force@171.69.52.109>
2014-01-13 16:01:58 -0800

    Updated hello resource to use @PathParam and @QueryParam tests with unit tests.

--- src/main/java/chirp/service/resources/HelloResource.java
+++ src/main/java/chirp/service/resources/HelloResource.java
@@ -2,14 +2,33 @@ package chirp.service.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+
+@Path("/greeting")
 public class HelloResource {
 
 	@GET
+	@Produces(MediaType.TEXT_PLAIN)
 	public String getHello() {
 
 		return "Hello!";
 	}
+	
+	@GET
+	@Path("/{name}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getHelloWithName(@PathParam("name") String name) {
+		return "Hello " + name + "!";
+	}
 
+	@GET
+	@Path("/withName")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getHelloWithNameQueryParam(@QueryParam("name") String name) {
+		return "Hello " + name + "!";
+	}
 }
--- src/test/java/chirp/service/resources/HelloResourceTest.java
+++ src/test/java/chirp/service/resources/HelloResourceTest.java
@@ -8,8 +8,21 @@ public class HelloResourceTest extends JerseyResourceTest<HelloResource> {
 
 	@Test
 	public void helloResourceMustSayHello() {
-		String hello = target("/hello").request().get(String.class);
+		String hello = target("/greeting").request().get(String.class);
 		assertEquals("Hello!", hello);
 	}
+	
+	@Test
+	public void helloResourceMustSayHelloWithName() {
+		String hello = target("/greeting/Cisco").request().get(String.class);
+		assertEquals("Hello Cisco!", hello);
+	}
+	
+	@Test
+	public void helloResourceMustSayHelloWithNameQueryParam() {
+		String hello = target("/greeting/withName").queryParam("name", "Siene").request().get(String.class);
+		assertEquals("Hello Siene!", hello);
+	}
+
 
 }
