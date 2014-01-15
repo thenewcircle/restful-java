c12e8179c16cd470efb2367a75e56f2e1fd8fab9
Gordon Force <Gordon Force@171.69.52.109>
2014-01-15 13:31:43 -0800

    Added support for self and posts structural links.

--- src/main/java/chirp/service/representations/UserRepresentation.java
+++ src/main/java/chirp/service/representations/UserRepresentation.java
@@ -1,5 +1,8 @@
 package chirp.service.representations;
 
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -7,30 +10,50 @@ import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 import chirp.model.User;
+import chirp.service.resources.UserResource;
 
 @XmlRootElement
 public class UserRepresentation {
 
 	@XmlElement
 	private String username;
+
 	@XmlElement
 	private String realname;
+
+	@XmlElement
+	private URI self;
+	
+	@XmlElement
+	private URI posts;
 
 	public UserRepresentation(User user) {
 		this.username = user.getUsername();
 		this.realname = user.getRealname();
+		
+		// http://localhost:8080/users/<username>
+		this.self = UriBuilder.fromResource(UserResource.class).path(username).build();
+		
+		// http://localhost:8080/posts/<username>
+		this.posts = UriBuilder.fromPath("/posts").path(username).build();
 	}
-	
+
 	public UserRepresentation() {
 		this.username = null;
 		this.realname = null;
+		this.posts = null;
+		this.self = null;
 	}
 
 	@JsonCreator()
-	public UserRepresentation(@JsonProperty("username") String username,
+	public UserRepresentation(@JsonProperty("self") URI self,
+			@JsonProperty("posts") URI posts,
+			@JsonProperty("username") String username,
 			@JsonProperty("realname") String realname) {
 		this.username = username;
 		this.realname = realname;
+		this.self = self;
+		this.posts = posts;
 	}
 
 	public String getUsername() {
@@ -39,6 +62,14 @@ public class UserRepresentation {
 
 	public String getRealname() {
 		return realname;
+	}
+
+	public URI getSelf() {
+		return self;
+	}
+
+	public URI getPosts() {
+		return posts;
 	}
 
 }
