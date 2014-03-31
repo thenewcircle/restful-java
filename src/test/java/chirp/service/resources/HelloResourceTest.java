024cb3fc7391fe7c7802ae31b4dc892d15da5771
Doug Bateman <doug@dougbateman.net>
2014-03-31 12:13:13 -0700

    Adding example with ClientBuilder.

--- src/test/java/chirp/service/resources/HelloResourceTest.java
+++ src/test/java/chirp/service/resources/HelloResourceTest.java
@@ -2,13 +2,18 @@ package chirp.service.resources;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.ws.rs.client.WebTarget;
+
 import org.junit.Test;
 
 public class HelloResourceTest extends JerseyResourceTest {
 
 	@Test
 	public void helloResourceMustSayHello() {
-		String hello = target("/hello").request().get(String.class);
+//		Client client = ClientBuilder.newClient();
+//		WebTarget root = client.target("http://localhost:8080/");
+		WebTarget root = super.target("/hello");
+		String hello = root.request().get(String.class);
 		assertEquals("Hello!", hello);
 	}
 
