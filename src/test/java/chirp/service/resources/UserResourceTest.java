4ca003d254b198f6a4cec59ef6ea7b765b67ceca
Doug Bateman <doug@dougbateman.net>
2014-04-01 14:18:16 -0700

    Create a Resource — Passing Information to Resources Part 3, Step 3

--- src/test/java/chirp/service/resources/UserResourceTest.java
+++ src/test/java/chirp/service/resources/UserResourceTest.java
@@ -1,29 +1,52 @@
 package chirp.service.resources;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Form;
 import javax.ws.rs.core.Response;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import chirp.model.UserRepository;
 
 public class UserResourceTest extends JerseyResourceTest {
 
+	@Before
+	public void clearDatabase() {
+		UserRepository.getInstance().clear();
+		UserRepository.getInstance().prepopulate();
+	}
+	
+	@Test
+	public void testGetUser() {
+		WebTarget root = super.target();
+		String realName = root.path("users").path("yoda").request().get(String.class);
+		Assert.assertEquals("Master Yoda", realName);
+	}
+
 	@Test
 	public void testCreateUser() {
-//		Client client = ClientBuilder.newClient();
-//		WebTarget root = client.target("http://localhost:8090/");
 		WebTarget root = super.target();
-		
 		Form form = new Form();
 		form.param("username", "dbateman");
 		form.param("realname", "Doug Bateman");
-		Entity<?> uploadData = Entity.form(form);
+		Entity<Form> uploadData = Entity.form(form);
 		Response response = root.path("users").request().post(uploadData);
-		Assert.assertEquals(204, response.getStatus());
+		Assert.assertEquals(201, response.getStatus());
+		String realName = root.path("users").path("dbateman").request().get(String.class);
+		Assert.assertEquals("Doug Bateman", realName);
 	}
 
+	@Test
+	public void testSaveUser() {
+		WebTarget root = super.target();
+		Entity<String> uploadData = Entity.text("Doug Bateman");
+		Response response = root.path("users").path("dbateman").request().put(uploadData);
+		Assert.assertEquals(201, response.getStatus());
+		String realName = root.path("users").path("dbateman").request().get(String.class);
+		Assert.assertEquals("Doug Bateman", realName);
+	}
 }
+
