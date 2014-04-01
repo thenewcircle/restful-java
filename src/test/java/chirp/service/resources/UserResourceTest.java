43048b97c79a71eac654afbeffa5bd84242877fe
Doug Bateman <doug@dougbateman.net>
2014-04-01 15:25:58 -0700

    Test case for JSon

--- src/test/java/chirp/service/resources/UserResourceTest.java
+++ src/test/java/chirp/service/resources/UserResourceTest.java
@@ -3,12 +3,14 @@ package chirp.service.resources;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import chirp.model.User;
 import chirp.model.UserRepository;
 
 public class UserResourceTest extends JerseyResourceTest {
@@ -20,10 +22,17 @@ public class UserResourceTest extends JerseyResourceTest {
 	}
 	
 	@Test
-	public void testGetUser() {
+	public void testGetUserAsText() {
 		WebTarget root = super.target();
-		String realName = root.path("users").path("yoda").request().get(String.class);
+		String realName = root.path("users").path("yoda").request().accept(MediaType.TEXT_PLAIN).get(String.class);
 		Assert.assertEquals("Master Yoda", realName);
+	}
+
+	@Test
+	public void testGetUserAsJSon() {
+		WebTarget root = super.target();
+		User user = root.path("users").path("yoda").request().accept(MediaType.APPLICATION_JSON).get(User.class);
+		Assert.assertEquals("Master Yoda", user.getRealname());
 	}
 
 	@Test
@@ -35,7 +44,7 @@ public class UserResourceTest extends JerseyResourceTest {
 		Entity<Form> uploadData = Entity.form(form);
 		Response response = root.path("users").request().post(uploadData);
 		Assert.assertEquals(201, response.getStatus());
-		String realName = root.path("users").path("dbateman").request().get(String.class);
+		String realName = root.path("users").path("dbateman").request().accept(MediaType.TEXT_PLAIN).get(String.class);
 		Assert.assertEquals("Doug Bateman", realName);
 	}
 
@@ -45,7 +54,7 @@ public class UserResourceTest extends JerseyResourceTest {
 		Entity<String> uploadData = Entity.text("Doug Bateman");
 		Response response = root.path("users").path("dbateman").request().put(uploadData);
 		Assert.assertEquals(201, response.getStatus());
-		String realName = root.path("users").path("dbateman").request().get(String.class);
+		String realName = root.path("users").path("dbateman").request().accept(MediaType.TEXT_PLAIN).get(String.class);
 		Assert.assertEquals("Doug Bateman", realName);
 	}
 }
