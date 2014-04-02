fe30eb10418ba8d214b61b63f233fdceded0ddfb
Gordon Force <gordon@thenewcircle.com>
2014-04-02 11:37:59 -0400

    UserCreate test case solution.

--- src/test/java/chirp/service/resources/UserResourceTest.java
+++ src/test/java/chirp/service/resources/UserResourceTest.java
@@ -1,12 +1,12 @@
 package chirp.service.resources;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Form;
 import javax.ws.rs.core.Response;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import chirp.model.User;
@@ -15,6 +15,11 @@ import chirp.model.UserRepository;
 public class UserResourceTest extends JerseyResourceTest<UserResource> {
 	
 	private UserRepository userRepository = UserRepository.getInstance();
+	
+	@Before
+	public void testInit() {
+		userRepository.clear();
+	}
 
 	@Test
 	public void createUserSuccess() {
@@ -29,6 +34,23 @@ public class UserResourceTest extends JerseyResourceTest<UserResource> {
 		// User userRead = target(response.getLocation().getPath()).request().get(User.class);
 	
 	}
+	
+	@Test
+	public void createDuplicateUserFail() {
+		Form userForm = new Form().param("realname","Gordon Force").param("username", "gordonff");
+
+		Response response = target("/user").request().post(Entity.form(userForm));
+		assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
+		
+		response = target("/user").request().post(Entity.form(userForm));
+		assertEquals(Response.Status.FORBIDDEN.getStatusCode(), response.getStatus());
+		
+		// You wan't to an object from the server -- User
+		//    the entity to read is in the previous response's location header
+		// User userRead = target(response.getLocation().getPath()).request().get(User.class);
+	
+	}
+
 	
 
 }
