ef2847603a59bd09a7ca1d2a43c76f70153af9bf
Gordon Force <gordon@thenewcircle.com>
2014-07-16 14:49:33 -0700

    Partial get user implementation

--- src/main/java/chirp/service/resources/UsersResource.java
+++ src/main/java/chirp/service/resources/UsersResource.java
@@ -1,16 +1,19 @@
 package chirp.service.resources;
 
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import chirp.model.DuplicateEntityException;
+import chirp.model.User;
 import chirp.model.UserRepository;
 
 @Path("/users")
@@ -31,6 +34,13 @@ public class UsersResource {
 			return Response.created(
 					UriBuilder.fromPath("/users").path(username).build())
 					.build();
+	}
+	
+	@GET
+	@Path("{username}")
+	@Produces(MediaType.APPLICATION_XML)
+	public User getUser(@PathParam("username") String username) {
+		return repo.getUser(username);
 	}
 
 }
