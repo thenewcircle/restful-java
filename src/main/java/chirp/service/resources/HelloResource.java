b0b609aa3609ed29fbc8985b1ab35f03ffe4e57b
student <student@localhost>
2014-10-07 11:42:28 -0700

    Solution to AM lab.

--- src/main/java/chirp/service/resources/HelloResource.java
+++ src/main/java/chirp/service/resources/HelloResource.java
@@ -2,14 +2,36 @@ package chirp.service.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
-@Path("/hello")
+@Path("/greeting")
 public class HelloResource {
 
+	// http://localhost:8080/hello?name=<somevalue>
 	@GET
-	public String getHello() {
-
-		return "Hello!";
+	public String getHelloWithName(@QueryParam("name") String helloName) {
+		
+		if (helloName == null) {
+			return "Hello!";
+		}
+		else {
+			return "Hello " + helloName + "!";
+		}
 	}
+
+	// http://localhost:8080/hello/<helloName>
+	@GET
+	@Path("{helloName}")
+	public String getHelloWithPathName(@PathParam("helloName") String helloName) {
+		
+		if (helloName == null)
+			throw new NullPointerException("path param not valid");
+
+		return "Hello " + helloName + "!";
+	}
+
+
+
 
 }
--- src/test/java/chirp/service/resources/HelloResourceTest.java
+++ src/test/java/chirp/service/resources/HelloResourceTest.java
@@ -8,8 +8,14 @@ public class HelloResourceTest extends JerseyResourceTest<HelloResource> {
 
 	@Test
 	public void helloResourceMustSayHello() {
-		String hello = target("/hello").request().get(String.class);
+		String hello = target("/greeting").request().get(String.class);
 		assertEquals("Hello!", hello);
+	}
+
+	@Test
+	public void helloResourceMustSayHelloWithPathParam() {
+		String hello = target("/greeting/Cisco").request().get(String.class);
+		assertEquals("Hello Cisco!", hello);
 	}
 
 }
