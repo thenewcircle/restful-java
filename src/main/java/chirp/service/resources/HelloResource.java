1b7fe7529252045cdea7e9ffe2559a785d7a8d21
Gordon Force Jr. <gordon@thenewcircle.com>
2014-10-18 15:50:27 -0700

    Solution for hello resource labs.

--- src/main/java/chirp/service/resources/HelloResource.java
+++ src/main/java/chirp/service/resources/HelloResource.java
@@ -2,14 +2,22 @@ package chirp.service.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
-@Path("/hello")
+@Path("/greetings")
 public class HelloResource {
 
 	@GET
-	public String getHello() {
-
-		return "Hello!";
+	public String getHello(@QueryParam("name") String name) {
+		return (name == null) ? "Hello!" : String.format("Hello, %s!", name);
+	}
+	
+	@GET
+	@Path("{name}")
+	public String getHelloAsPath(@PathParam("name") String name) {
+		
+		return String.format("Hello, %s!", name);
 	}
 
 }
--- src/test/java/chirp/service/resources/HelloResourceTest.java
+++ src/test/java/chirp/service/resources/HelloResourceTest.java
@@ -7,9 +7,23 @@ import org.junit.Test;
 public class HelloResourceTest extends JerseyResourceTest {
 
 	@Test
-	public void helloResourceMustSayHello() {
-		String hello = target("/hello").request().get(String.class);
+	public void testGetHello() {
+		String hello = target("/greetings").request().get(String.class);
 		assertEquals("Hello!", hello);
+	}
+
+	@Test
+	public void testGetHelloSomebodyAsParameter() {
+		String hello = target("/greetings").queryParam("name", "NewCircle")
+				.request().get(String.class);
+		assertEquals("Hello, NewCircle!", hello);
+	}
+
+	@Test
+	public void testGetHelloSomebodyAsPath() {
+		String hello = target("/greetings/NewCircle").request().get(
+				String.class);
+		assertEquals("Hello, NewCircle!", hello);
 	}
 
 }
