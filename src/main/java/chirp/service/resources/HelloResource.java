04249c13f85c823ccd17965b81b1784f941bf827
gordonforce <gordon@thenewcircle.com>
2015-04-14 17:23:03 -0700

    End of day one. Passing in parameters from paths and querys.

--- src/main/java/chirp/service/resources/HelloResource.java
+++ src/main/java/chirp/service/resources/HelloResource.java
@@ -2,14 +2,38 @@ package chirp.service.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class HelloResource {
 
 	@GET
-	public String getHello() {
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getHello(@QueryParam("name") String uriNameParam) {
 
-		return "Hello!";
+		if (uriNameParam != null)
+			return "Hello " + uriNameParam + "!";
+		else
+			return "Hello!";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	@Path("{name}")
+	public String anotherGetHello(@PathParam("name") String name) {
+		
+		StringBuilder builder = new StringBuilder("<html><body><h1>Hello");
+		
+		if (name.length() > 0) {
+			builder.append(" ").append(name);
+		}
+		
+		builder.append("!</h1></body></html>");
+		
+		return builder.toString();
 	}
 
 }
--- src/test/java/chirp/service/resources/HelloResourceTest.java
+++ src/test/java/chirp/service/resources/HelloResourceTest.java
@@ -2,14 +2,24 @@ package chirp.service.resources;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.ws.rs.core.MediaType;
+
 import org.junit.Test;
 
 public class HelloResourceTest extends JerseyResourceTest {
 
 	@Test
 	public void helloResourceMustSayHello() {
-		String hello = target("/hello").request().get(String.class);
+		String hello = target("/hello").request()
+				.header("Accept", MediaType.TEXT_PLAIN_TYPE).get(String.class);
 		assertEquals("Hello!", hello);
+	}
+
+	@Test
+	public void helloResourceGreetsByName() {
+		String hello = target("/hello/Cisco")
+				.request().header("Accept", MediaType.TEXT_HTML_TYPE).get(String.class);
+		assertEquals("<html><body><h1>Hello Cisco!</h1></body></html>", hello);
 	}
 
 }
