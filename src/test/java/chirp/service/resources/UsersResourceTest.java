902704a7375b0697cb40526d79ace52e0052635b
gordonforce <gordon@thenewcircle.com>
2015-06-24 14:14:53 -0700

    Exception Mapper solution

--- src/test/java/chirp/service/resources/UsersResourceTest.java
+++ src/test/java/chirp/service/resources/UsersResourceTest.java
@@ -12,39 +12,36 @@ import org.junit.Test;
 import chirp.model.UserRepository;
 
 public class UsersResourceTest extends JerseyResourceTest {
-	
+
+	private Response createBobStudent(Response.Status expectedStatus) {
+		Response response = target("/users").request().post(
+				Entity.form(new Form().param("realname", "Bob Student")
+						.param("username", "student")));
+
+		assertEquals(expectedStatus.getStatusCode(), response.getStatus());
+
+		return response;
+	}
+
 	@Before
 	public void testSetup() {
 		UserRepository.getInstance().clear();
 	}
-	
+
 	@Test
 	public void createUserSuccess() {
-		
-		Form form = new Form().param("realname", "Bob Student").param("username", "student");
-		
-		Response response = target("/users").request().post(Entity.form(form));
-		
-		assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
-		assertEquals("http://localhost:9998/users/student", response.getLocation().toString());
-		
+
+		Response response = createBobStudent(Response.Status.CREATED);
+		assertEquals("http://localhost:9998/users/student", response
+				.getLocation().toString());
+
 	}
-	
+
 	@Test
 	public void createSameUserFails() {
-		
-		Form form = new Form().param("realname", "Bob Student").param("username", "student");
-		
-		Response response = target("/users").request().post(Entity.form(form));
-		
-		assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
-		assertEquals("http://localhost:9998/users/student", response.getLocation().toString());
-		
-		form = new Form().param("realname", "Bob Student").param("username", "student");	
-		response = target("/users").request().post(Entity.form(form));
-		assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
 
-		
+		createBobStudent(Response.Status.CREATED);
+		createBobStudent(Response.Status.BAD_REQUEST);
 	}
 
 }
