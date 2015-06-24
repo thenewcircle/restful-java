db419ff58307ccc39c7af47aa5c6136c0c72f85d
gordonforce <gordon@thenewcircle.com>
2015-06-24 12:26:12 -0700

    Added lab assignment and setup for lunch on day two.

--- src/test/java/chirp/service/resources/UsersResourceTest.java
+++ src/test/java/chirp/service/resources/UsersResourceTest.java
@@ -1,14 +1,22 @@
 package chirp.service.resources;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Form;
 import javax.ws.rs.core.Response;
 
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
+import chirp.model.UserRepository;
+
 public class UsersResourceTest extends JerseyResourceTest {
+	
+	@Before
+	public void testSetup() {
+		UserRepository.getInstance().clear();
+	}
 	
 	@Test
 	public void createUserSuccess() {
@@ -21,4 +29,22 @@ public class UsersResourceTest extends JerseyResourceTest {
 		assertEquals("http://localhost:9998/users/student", response.getLocation().toString());
 		
 	}
+	
+	@Test
+	public void createSameUserFails() {
+		
+		Form form = new Form().param("realname", "Bob Student").param("username", "student");
+		
+		Response response = target("/users").request().post(Entity.form(form));
+		
+		assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
+		assertEquals("http://localhost:9998/users/student", response.getLocation().toString());
+		
+		form = new Form().param("realname", "Bob Student").param("username", "student");	
+		response = target("/users").request().post(Entity.form(form));
+		assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+
+		
+	}
+
 }
