f66ff8a80fff0508e87a52236d6773246fdbfe8e
Jacob D. Parr <me@jacobparr.com>
2015-06-29 23:57:41 -0700

    Fix: Test fixture was scanning packages, not using the jax-rs application class.

--- chirp-app/src/test/java/com/example/chirp/app/resources/ResourceTestSupport.java
+++ chirp-app/src/test/java/com/example/chirp/app/resources/ResourceTestSupport.java
@@ -3,16 +3,16 @@ package com.example.chirp.app.resources;
 import javax.ws.rs.core.Application;
 
 import ch.qos.logback.classic.Level;
+
+import com.example.chirp.app.ChirpApplication;
 import com.example.chirp.app.LogbackUtil;
-import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 
 public abstract class ResourceTestSupport extends JerseyTest {
 
 	@Override
 	protected Application configure() {
-    LogbackUtil.initLogback(Level.WARN);
-
-		return new ResourceConfig().packages("com.example.chirp.app");
+		LogbackUtil.initLogback(Level.WARN);
+		return new ChirpApplication();
 	}
 }
