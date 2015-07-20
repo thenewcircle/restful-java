6f484f0e2c7b8677755061df703c8f92461cd2bb
Jacob D. Parr <me@jacobparr.com>
2015-07-20 15:36:46 -0700

    Header params

--- src/main/java/com/example/chirp/app/GreetingsResource.java
+++ src/main/java/com/example/chirp/app/GreetingsResource.java
@@ -1,16 +1,33 @@
 package com.example.chirp.app;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/greetings")
 public class GreetingsResource {
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String sayHello() {
-		return "Hello!";
+	public Response sayHello(@DefaultValue("dude") @QueryParam("name") String someName, @HeaderParam("X-NewCircle-Echo") String echoHeaderValue) {
+
+		String retVal = "Hello " + someName + "!";
+
+		String echo = String.format("%s, %s, %s", echoHeaderValue, echoHeaderValue, echoHeaderValue);
+
+		return Response.ok(retVal).header("X-NewCircle-Echo-Response", echo).build();
+	}
+
+	@Path("/{name}")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayHelloWithPathParam(@PathParam("name") String someName) {
+		return "Hello " + someName + "!";
 	}
 }
--- src/test/java/com/example/chirp/app/resources/GreetingsResourceTest.java
+++ src/test/java/com/example/chirp/app/resources/GreetingsResourceTest.java
@@ -12,6 +12,31 @@ public class GreetingsResourceTest extends ResourceTestSupport {
 		Response response = target("/greetings").request().get();
 		Assert.assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
 		String text = response.readEntity(String.class);
-		Assert.assertEquals("Hello!", text);
+		Assert.assertEquals("Hello dude!", text);
+	}
+
+	@Test
+	public void testSayHelloWithQueryParam() {
+		Response response = target("/greetings").queryParam("name", "Tom").request().get();
+		Assert.assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+		String text = response.readEntity(String.class);
+		Assert.assertEquals("Hello Tom!", text);
+	}
+
+	@Test
+	public void testSayHelloWithPathParam() {
+		Response response = target("/greetings").path("Tom").request().get();
+		Assert.assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+		String text = response.readEntity(String.class);
+		Assert.assertEquals("Hello Tom!", text);
+	}
+
+	@Test
+	public void testEchoHeader() {
+		Response response = target("/greetings").request().header("X-NewCircle-Echo", "Tom").get();
+
+		Assert.assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+		String text = response.getHeaderString("X-NewCircle-Echo-Response");
+		Assert.assertEquals("Tom, Tom, Tom", text);
 	}
 }
