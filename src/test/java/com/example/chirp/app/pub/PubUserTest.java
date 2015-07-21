f3d553b23c27a8d8e1ed712149b9325f248d277f
Jacob D. Parr <me@jacobparr.com>
2015-07-21 15:34:00 -0700

    Test for translating XML

--- src/test/java/com/example/chirp/app/pub/PubUserTest.java
+++ src/test/java/com/example/chirp/app/pub/PubUserTest.java
@@ -4,17 +4,29 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 public class PubUserTest {
 
 	private ObjectMapper objectMapper = new ObjectMapper();
+	private XmlMapper xmlMapper = new XmlMapper();
 
 	@Test
-	public void testTranslation() throws Exception {
-
+	public void testTranslationJson() throws Exception {
 		PubUser oldUser = new PubUser("mickey.mouse", "Mickey Mouse");
 		String json = objectMapper.writeValueAsString(oldUser);
 		PubUser newUser = objectMapper.readValue(json, PubUser.class);
+
+		Assert.assertEquals(oldUser.getUsername(), newUser.getUsername());
+		Assert.assertEquals(oldUser.getRealname(), newUser.getRealname());
+	}
+
+	@Test
+	public void testTranslationXml() throws Exception {
+
+		PubUser oldUser = new PubUser("mickey.mouse", "Mickey Mouse");
+		String json = xmlMapper.writeValueAsString(oldUser);
+		PubUser newUser = xmlMapper.readValue(json, PubUser.class);
 
 		Assert.assertEquals(oldUser.getUsername(), newUser.getUsername());
 		Assert.assertEquals(oldUser.getRealname(), newUser.getRealname());
