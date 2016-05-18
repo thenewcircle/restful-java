fed0e31087e2c765ec789d5e618fbdf6ff24169d
Jacob D. Parr <me@jacobparr.com>
2016-05-18 10:48:51 -0700

    Checkpoit

--- src/main/java/com/example/chirp/app/ChirpApplication.java
+++ src/main/java/com/example/chirp/app/ChirpApplication.java
@@ -5,8 +5,14 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import com.example.chirp.app.stores.InMemoryUserStore;
+import com.example.chirp.app.stores.UserStore;
+
 //@ApplicationPath("/")
 public class ChirpApplication extends Application {
+
+  public static UserStore USER_STORE = new InMemoryUserStore(true);
+  
   private Set<Class<?>> classes = new HashSet<>();
 
   public ChirpApplication() {
