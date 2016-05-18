0c60b2baeb953b9b3537328289070b1805591e2b
Jacob D. Parr <me@jacobparr.com>
2016-05-18 15:05:42 -0700

    Add pub utils

--- src/main/java/com/example/chirp/app/pub/PubUtils.java
+++ src/main/java/com/example/chirp/app/pub/PubUtils.java
@@ -1,5 +1,31 @@
 package com.example.chirp.app.pub;
 
-public class PubUtils {
+import java.net.URI;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import javax.ws.rs.core.UriInfo;
+
+import com.example.chirp.app.kernel.User;
+
+public class PubUtils {
+  public static PubUser toPubUser(UriInfo uriInfo, User user) {
+
+    Map<String,URI> links = new LinkedHashMap<>();
+
+    links.put("self", uriInfo.getBaseUriBuilder()
+                             .path("users")
+                             .path(user.getUsername())
+                             .build());
+
+    links.put("chirps", uriInfo.getBaseUriBuilder()
+      .path("users")
+      .path(user.getUsername())
+      .path("chirps").build());
+
+    return new PubUser(links, 
+                       user.getUsername(), 
+                       user.getRealName());
+  }
 }
+
---
+++ src/test/java/com/example/chirp/app/pub/PubUtilsTest.java
@@ -0,0 +1,26 @@
+package com.example.chirp.app.pub;
+
+import java.net.URI;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.example.chirp.app.kernel.User;
+
+public class PubUtilsTest {
+
+  @Test  
+  public void testToPubUser() {
+    User user = new User("mickey", "Mickey Mouse");
+    PubUser pubUser = PubUtils.toPubUser(new MockUriInfo(), user);
+
+    Assert.assertEquals(user.getUsername(), pubUser.getUsername());
+    Assert.assertEquals(user.getRealName(), pubUser.getRealName());
+
+    Assert.assertEquals(URI.create("http://mock.com/users/mickey"),
+                        pubUser.getLinks().get("self"));
+
+    Assert.assertEquals(URI.create("http://mock.com/users/mickey/chirps"),
+                        pubUser.getLinks().get("chirps"));
+  }
+}
