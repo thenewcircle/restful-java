0d2dcdbadf8f632ce13228219e0d4d8d58ea2187
Jacob D. Parr <me@jacobparr.com>
2016-05-18 14:34:44 -0700

    @BeanParam

--- src/main/java/com/example/chirp/app/resources/CreateUserRequest.java
+++ src/main/java/com/example/chirp/app/resources/CreateUserRequest.java
@@ -1,5 +1,39 @@
 package com.example.chirp.app.resources;
 
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
 public class CreateUserRequest {
 
+  private @PathParam("username") String username; 
+  private @FormParam("realName") String fullName;
+  private @Context UriInfo uriInfo;
+
+  public String getUsername() {
+    return username;
+  }
+  public void setUsername(String username) {
+    this.username = username;
+  }
+  public String getFullName() {
+    return fullName;
+  }
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+  public UriInfo getUriInfo() {
+    return uriInfo;
+  }
+  public void setUriInfo(UriInfo uriInfo) {
+    this.uriInfo = uriInfo;
+  }
+  
+  public void validate() {
+    if (getUsername().contains(" ")) {
+      throw new BadRequestException("The user name cannot contain spaces.");
+    }
+  }
 }
--- src/main/java/com/example/chirp/app/resources/UserResource.java
+++ src/main/java/com/example/chirp/app/resources/UserResource.java
@@ -3,38 +3,33 @@ package com.example.chirp.app.resources;
 import java.net.URI;
 
 import javax.ws.rs.BadRequestException;
-import javax.ws.rs.FormParam;
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 
 import com.example.chirp.app.ChirpApplication;
 import com.example.chirp.app.kernel.User;
-import com.example.chirp.app.kernel.exceptions.DuplicateEntityException;
 
 @Path("/users")
 public class UserResource {
 
   @PUT
   @Path("/{username}")
-  public Response createUser(@PathParam("username") String username, 
-                             @FormParam("realName") String fullName,
-                             @Context UriInfo uriInfo) {
+  public Response createUser(@BeanParam CreateUserRequest request) {
 
-    if (username.contains(" ")) {
-      throw new BadRequestException("The user name cannot contain spaces.");
-    }
+    request.validate();
     
-    User user = ChirpApplication.USER_STORE.createUser(username, fullName);
+    User user = ChirpApplication.USER_STORE.createUser(
+        request.getUsername(), 
+        request.getFullName());
 
     //URI uri = uriInfo.getAbsolutePathBuilder();
-    URI uri = uriInfo.getBaseUriBuilder()
+    URI uri = request.getUriInfo().getBaseUriBuilder()
         .path("/users")
-        .path(username)
+        .path(request.getUsername())
         .build();
 
     return Response.created(uri).build();
